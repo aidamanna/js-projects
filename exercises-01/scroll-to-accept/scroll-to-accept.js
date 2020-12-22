06d78074571e29cfc0dbe4c157979732a6c1a4ed
Aida Manna <aidamanna@gmail.com>
2020-12-22 16:27:33 +0100

    Scroll to accept exercise

---
+++ exercises-01/scroll-to-accept/scroll-to-accept.js
@@ -0,0 +1,18 @@
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+function observerCallback(payload) {
+    if (payload[0].intersectionRatio === 1) {
+        button.disabled = false;
+    } else {
+        // stop the observer
+        observer.unobserve(terms.lastElementChild);
+    }
+}
+
+const observer = new IntersectionObserver(observerCallback, {
+    root: terms,
+    threshold: 1
+});
+observer.observe(terms.lastElementChild);
