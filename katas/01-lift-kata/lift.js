96a339d30b6fa683fabc5b0aab798cfaf5c90a86
Aida Manna <aidamanna@gmail.com>
2021-01-28 18:28:58 +0100

    Add sound when lift arrives floor

--- katas/01-lift-kata/lift.js
+++ katas/01-lift-kata/lift.js
@@ -1,21 +1,19 @@
-console.log("it works!");
-
 const lift = document.querySelector('.lift');
-
-buttonFloor0 = document.querySelector('.floor0 button');
-buttonFloor1 = document.querySelector('.floor1 button');
-buttonFloor2 = document.querySelector('.floor2 button');
-buttonFloor3 = document.querySelector('.floor3 button');
-
+const callButtons = document.querySelectorAll('button');
+const audio = new Audio('lift-arrived.mp3');
 
 function goToFloor(floorNumber) {
-    console.log(`calling floor ${floorNumber}`)
-    const classesToRemove = ['to-floor-0', 'to-floor-1', 'to-floor-2', 'to-floor-3'];
+    const classesToRemove = ['floor0', 'floor1', 'floor2', 'floor3'];
     lift.classList.remove(...classesToRemove);
-    lift.classList.add(`to-floor-${floorNumber}`);
+    lift.classList.add(`floor${floorNumber}`);
 }
 
-buttonFloor0.addEventListener('click', () => goToFloor(0));
-buttonFloor1.addEventListener('click', () => goToFloor(1));
-buttonFloor2.addEventListener('click', () => goToFloor(2));
-buttonFloor3.addEventListener('click', () => goToFloor(3));
+function liftArrivedSound() {
+    audio.play();
+}
+
+callButtons.forEach(callButton => { 
+    callButton.addEventListener('click', () => goToFloor(callButton.dataset.floorNumber));
+});
+
+window.addEventListener('transitionend', liftArrivedSound);
