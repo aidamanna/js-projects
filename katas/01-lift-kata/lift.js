c75aa386410369060cdd17d3fc839c3ab8b65785
Aida Manna <aidamanna@gmail.com>
2021-01-29 14:02:58 +0100

    Add lift floor numbre on each floor

--- katas/01-lift-kata/lift.js
+++ katas/01-lift-kata/lift.js
@@ -1,19 +1,30 @@
 const lift = document.querySelector('.lift');
 const callButtons = document.querySelectorAll('button');
+const liftPannels = document.querySelectorAll('.lift-pannel');
 const audio = new Audio('lift-arrived.mp3');
+
+let liftFloor = 0;
 
 function goToFloor(floorNumber) {
     const classesToRemove = ['floor0', 'floor1', 'floor2', 'floor3'];
     lift.classList.remove(...classesToRemove);
     lift.classList.add(`floor${floorNumber}`);
+    liftFloor = floorNumber;
 }
 
-function liftArrivedSound() {
+function liftArrivedToFloor() {
+    setLiftFloor();
     audio.play();
+}
+
+function setLiftFloor() {
+    liftPannels.forEach(liftPannel => liftPannel.innerHTML = `Lift in floor ${liftFloor}`);
 }
 
 callButtons.forEach(callButton => { 
     callButton.addEventListener('click', () => goToFloor(callButton.dataset.floorNumber));
 });
 
-window.addEventListener('transitionend', liftArrivedSound);
+window.addEventListener('transitionend', liftArrivedToFloor);
+
+setLiftFloor();
